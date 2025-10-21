b8fcdb5d3196b37eaa2d63195b46aaef789f7f0a
advaitong <advaitnair1111@gmail.com>
2025-10-21 19:23:10 +0530

    frontend work

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -1,15 +1,68 @@
-export function getAllNotes(req, res) {
-    res.status(200).send("you js fetched all notes");
+import Note from "../../models/Note.js";
+
+export async function getAllNotes(req, res) {
+  try {
+    const notes = await Note.find().sort({ createdAt: -1 });
+    res.status(200).json(notes);
+  } catch (error) {
+    console.log("error in getAllNotes controller: ", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 }
 
-export function createNote(req, res) {
-    res.status(200).send("note created succesfully");
+export async function getNoteById(req, res) {
+  try {
+      const note = await Note.findById(req.params.id);
+      if (!note) return res.status(404).json({ message: "Note not found!" });
+      res.json(note);
+  } catch (error) {
+    console.log("error in getNoteById controller: ", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 }
 
-export function updateNote(req, res) {
-    res.status(200).send("note updated successfully");
+export async function createNote(req, res) {
+  try {
+    const { title, content } = req.body;
+    const note = new Note({ title, content });
+
+    const savedNote = await note.save();
+    res.status(201).json(savedNote);
+  } catch (error) {
+    console.log("error in createNote controller: ", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 }
 
-export function deleteNote(req, res) {
-    res.status(200).send("note deleted successfully");
+export async function updateNote(req, res) {
+  try {
+    const { title, content } = req.body;
+    const updatedNote = await Note.findByIdAndUpdate(req.params.id, {
+      title,
+      content,
+    });
+    if (!updatedNote) {
+      return res.status(400).json({ message: "note not found" });
+    }
+
+    res.status(201).json(updatedNote);
+  } catch (error) {
+    console.log("error in updateNote controller: ", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
+
+export async function deleteNote(req, res) {
+  try {
+    const { title, content } = req.body;
+    const deletedNote = await Note.findByIdAndDelete(req.params.id);
+    if (!deletedNote) {
+      return res.status(400).json({ message: "note not found" });
+    }
+
+    res.status(201).json({ message: "Note deleted successfully!" });
+  } catch (error) {
+    console.log("error in deleteNote controller: ", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 }
---
+++ backend/src/middleware/rateLimiter.js
@@ -0,0 +1,20 @@
+import rateLimit from "../config/upstash.js";
+
+const rateLimiter = async (req, res, next) => {
+    try {
+        const { success } = await rateLimit.limit(userid)
+        
+        if (!success) {
+            return res.status(429).json({
+                message:"too many requests, try again later!"
+            })
+        }
+
+        next()
+    } catch (error) {
+        console.log("rate limit error", error)
+        next(error)
+    }
+}
+
+export default rateLimiter;
