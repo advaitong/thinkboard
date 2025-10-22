9b627efd536fe39e7333ba3363f1b89352d5be5a
advaitong <advaitnair1111@gmail.com>
2025-10-22 12:16:58 +0530

    Final commit!

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
+import cors from "cors";
 import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
@@ -9,13 +10,18 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001;
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 app.use(express.json());
 app.use(rateLimiter);
 
-app.use("/api/notes", notesRoutes);  
+app.use("/api/notes", notesRoutes);
 
 connectDB().then(() => {
   app.listen(PORT, () => {
     console.log("server started on PORT", PORT);
-  });  
+  });
 });
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import { Route, Routes } from 'react-router'
-import HomePage from './pages/HomePage'
-import CreatePage from './pages/CreatePage'
-import NoteDetailPage from './pages/NoteDetailPage'
+import React from "react";
+import { Route, Routes } from "react-router";
+import HomePage from "./pages/HomePage";
+import CreatePage from "./pages/CreatePage";
+import NoteDetailPage from "./pages/NoteDetailPage";
 
 const App = () => {
   return (
-    <div data-theme="dim">
+    <div data-theme="forest" className="relative h-full w-full">
       <Routes>
-        <Route path='/' element={<HomePage/>}/>
-        <Route path='/create' element={<CreatePage/>}/>
-        <Route path='/note/:id' element={<NoteDetailPage/>}/>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+        <Route path="/note/:id" element={<NoteDetailPage />} />
       </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
